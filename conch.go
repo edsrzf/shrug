cc17bcb2bcd174c84dac26f1be017ed2954b56fa
Evan Shaw <edsrzf@gmail.com>
2012-06-18 18:37:22 +1200

    Interactive command line

--- conch.go
+++ conch.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/edsrzf/fineline"
+)
+
 func main() {
+	ctx := newCtx()
+	ctx.stdin = os.Stdin
+	ctx.stdout = os.Stdout
+	ctx.stderr = os.Stderr
+
+	l := fineline.NewLineReader()
+	l.Prompt = "$ "
+	l.SetMaxHistory(10)
+
+	for {
+		str, err := l.Read(nil)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("error", err)
+			} else {
+				fmt.Println()
+			}
+			break
+		}
+		parser := newParser(str + "\n")
+		cmd := parser.parseCommand()
+		cmd.exec(nil, ctx)
+	}
 }
